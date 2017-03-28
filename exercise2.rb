c3bbfcdec5ec9cbfbd6578377cb86fdb05daacaf
jpileto <jp.ileto@icloud.com>
2017-03-27 22:59:43 -0400

    Done with exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+# Good tip for a $55 meal
+
+tip = 55 * 0.15
+puts "#{tip}"
+
+# Adding a string and an integer
+
+i = 5
+s = "10"
+total = i + s.to_i
+puts "#{total}"
+
+# Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+product = 45628 * 7839
+puts "The answer to your riddle is #{product}."
+
+# What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+(10 < 20 && 30 < 20) || !(10 == 11)
+puts false
