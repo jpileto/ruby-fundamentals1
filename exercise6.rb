c85be93e21373d8afc3da523cafebe5a3d2527bc
jpileto <jp.ileto@icloud.com>
2017-03-29 20:51:14 -0400

    done with exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,16 @@
 distance = 0
-while distance >= 0
+while distance > 0
   puts "Time for your daily workout. Which mode would you like to take?"
   option = gets.chomp
   if option == "walk"
-    distance += 1
-    puts "You are #{distance} km away from home."
+    walk = distance += 1
+    puts "You are #{walk} km away from home."
   else option == "run"
-    distance += 5
-    puts "You are #{distance} km away from home."
-      break if option == "go home"
+    run = distance += 5
+    puts "You are #{run} km away from home."
+  break if option == "go home"
+    puts "You are going home now."
+
   end
 end
 
